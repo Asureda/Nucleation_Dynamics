969e5ee1fc407665fa5ea186c19721c7e2740e9f
Alexandre Sureda Croguennoc <61150333+Asureda@users.noreply.github.com>
2024-02-16 23:44:31 +0100

    Delete build/lib directory

--- build/lib/Nucleation_Dynamics/__init__.py
+++
@@ -1,8 +0,0 @@
-from .cluster_properties import ClusterPhysics
-from .dynamics import OpenClusterDynamics
-from .closed_dynamics import ClosedClusterDynamics
-from .scipy_dynamics import ScipyClusterDynamics
-from .julia_dynamics import JuliaClusterDynamics
-from .hon import HON
-
-__version__ = '0.1.0-dev'
